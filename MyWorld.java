a694471be18cb9c2679f1a3432ca03e8dea454f3
Brauer Anna <abrauer@wamas.com>
2019-01-16 10:09:33 +0100

    health bar added

--- MyWorld.java
+++ MyWorld.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
+    public Bar bar = new Bar("Player 1", "health Points", 25, 100);
 
     /**
      * Constructor for objects of class MyWorld.
@@ -17,26 +18,29 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 800, 1);
+        addObject(bar, 125, 40);
         spawn();
     }
+
     public void pflanzeGras()
     {
         for(int i=0; i<1000; i++) {
-             for(int k=0; k<800; k++) {
+            for(int k=0; k<800; k++) {
                 Gras gras = new Gras();
                 addObject(gras, i, k);
             }
         }
     }
+
     public void spawn() {
         int randomX = 0;
         int randomY = 0;
         int randomAnzahl = 0;
-        
+
         randomX = (int)(Math.random()*1000);
         randomY = (int)(Math.random()*800);
         randomAnzahl = (int)(Math.random()*10);
-        
+
         addObject(new Mensch(),randomX,randomY);
         addObject(new Spear(), randomX,100);
         for (int i = 0; i < randomAnzahl; i++ ) {
@@ -62,9 +66,23 @@ public class MyWorld extends World
             randomY = (int)(Math.random()*800);
             addObject(new Pig(),randomX,randomY);
         }
-        
+
         //if (human.died == 1) {
         //    addObject(new Died(),500,400);
         //}
     }
+    
+    // public void healthbar()
+    // {
+        // if (bar.getValue() == bar.getMinimumValue())
+        // {
+            // if (getObjects(GameOver.class).isEmpty()) showGameOver();
+            // return;
+        // }
+    // }
+    
+    // private void showGameOver()
+    // {
+        // addObject(new GameOver(), getWidth() / 2, getHeight() / 2);
+    // }
 }
