820b01420dc7844a1f67c7280dd0affc107ed7e0
Wehrheim <pwe@dbh.local>
2019-01-17 14:29:20 +0100

    MORE MORE MORE

---
+++ Lightsaber_red.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Lightsaber_red here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lightsaber_red extends Weapon
+{
+    /**
+     * Act - do whatever the Lightsaber_red wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
